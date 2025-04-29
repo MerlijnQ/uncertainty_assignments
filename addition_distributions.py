515cf3b97ad2bb07c2a72798125f82d7187c7fa5
MerlijnQ <m.q.mulder@student.rug.nl>
2025-04-29 15:15:28 +0200

    Generation of more samples and concatenation of graphs

--- addition_distributions.py
+++ addition_distributions.py
@@ -21,7 +21,7 @@ def fZ(z):
     # Convolution of the two distributions
     # fZ(z) = ∫ fX(x) * fY(z - x) dx
     # where fX is the Gaussian distribution and fY is the uniform distribution
-    x = np.linspace(mean_X_1 - 5*SD_X_1, mean_X_1 + 5*SD_X_1, 1000)
+    x = np.linspace(mean_X_1 - 5*SD_X_1, mean_X_1 + 5*SD_X_1, 10000)
 
     dx = x[1] - x[0]  # Step size for numerical integration
     integral = []
@@ -32,21 +32,23 @@ def fZ(z):
 
 def main():
 
-    X_1 = np.random.normal(mean_X_1, SD_X_1, 1000) # Height of 8 week olds: https://cdn.who.int/media/docs/default-source/child-growth/child-growth-standards/indicators/length-height-for-age/sft_lhfa_boys_z_0_13.pdf?sfvrsn=531f87ad_9
+    np.random.seed(42)
+
+    X_1 = np.random.normal(mean_X_1, SD_X_1, 10000) # Height of 8 week olds: https://cdn.who.int/media/docs/default-source/child-growth/child-growth-standards/indicators/length-height-for-age/sft_lhfa_boys_z_0_13.pdf?sfvrsn=531f87ad_9
 
     #a = mu - sigma * np.sqrt(3)
     #b = 2 * mu - a
 
-    X_2 = np.random.uniform(lower_bound, upper_bound, 1000) # Smiling times of 8 week olds: https://openstax.org/books/statistics/pages/5-2-the-uniform-distribution
+    X_2 = np.random.uniform(lower_bound, upper_bound, 10000) # Smiling times of 8 week olds: https://openstax.org/books/statistics/pages/5-2-the-uniform-distribution
 
     y = X_1 + X_2
 
     plt.hist(y, bins=50, density=True, alpha=0.7, color='g', label='Distribution of concatenated samples')
-    plt.show()
+    #plt.show()
 
     #fZ (z) = ∫ fX (x)fY (z − x)dx -> Convolution of the two distributions
 
-    z = np.linspace(min(y), max(y), 1000)
+    z = np.linspace(min(y), max(y), 10000)
     plt.plot(z, fZ(z), color='r', label='Convolution of the two distributions')
     plt.xlabel('Sum of variables')
     plt.ylabel('Density')
