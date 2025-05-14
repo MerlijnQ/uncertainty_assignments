d4b2893661a1a59662a32d40683add79fc7af915
BlokIan <i.d.blok@student.rug.nl>
2025-05-14 18:10:41 +0200

    Debugging
    Imrpoved model, some model training changes, added support for cuda.
    
    TODO
    fix function dropout_inference
    add val set when training (is this possible in phase 2?)

--- Disentangling_uncertainty.py
+++ Disentangling_uncertainty.py
@@ -1,71 +1,95 @@
 import torch
-from torch import nn
+import statistics
 import numpy as np
+from torch import nn
+from tqdm import tqdm
 from torch.nn import functional as F
+from torch.utils.tensorboard import SummaryWriter
 
 Mean = 0.0
 SD_1 = 0.19
-SD_2 = 0.
+SD_2 = 0.33
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
 
 class drop_connect(nn.Linear):
     def __init__(self, in_features, out_features, bias=True, p=0.2):
-        super(drop_connect, self).__init__(in_features, out_features, bias)
+        super().__init__(in_features=in_features, out_features=out_features, bias=bias)
         self.p = p
-        self.force_drop = True
+        self.inference = False
 
     def forward(self, x):
-        if self.training or self.force_drop:
+        if self.inference:
             weight = F.dropout(self.weight, p=self.p, training=True)
         else:
             weight = self.weight
         return F.linear(x, weight, self.bias)
 
+
 class regression_model(nn.Module):
     def __init__(self):
-        super(regression_model, self).__init__()
+        super().__init__()
         self.regression_layer = nn.Sequential(
-            F.relu(drop_connect(1, 32, p=0.2)),
-            F.relu(drop_connect(32, 32, p=0.2)),
+            drop_connect(1, 32, p=0.2),
+            nn.ReLU(),
+            drop_connect(32, 32, p=0.2),
+            nn.ReLU(),
         )
 
         self.mean_head = nn.Linear(32, 1)
         self.variance_head = nn.Linear(32, 1)
 
-
     def forward(self, x):
         x = self.regression_layer(x)
         mean = self.mean_head(x)
-        variance = self.variance_head(x)
+        variance = F.softplus(self.variance_head(x))
         return mean, variance
 
+
 def train_model(model, x, y, fase_1=True, epochs=100, learning_rate=0.01):
+    print(f"Training with {"MSE" if fase_1 else "Gaussian NLL"}")
+    writer = SummaryWriter(log_dir="logs")
+
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
     criterion = nn.MSELoss()
     criterion_2 = nn.GaussianNLLLoss()
 
-
     if fase_1:
         for param in model.variance_head.parameters():
             param.requires_grad = False
+    else:
+        for param in model.variance_head.parameters():
+            param.requires_grad = True
 
-    x_tensor = torch.tensor(x, dtype=torch.float32).view(-1, 1)
-    y_tensor = torch.tensor(y, dtype=torch.float32).view(-1, 1)
+    x_tensors = torch.tensor(x, dtype=torch.float32).view(-1, 1).to(device=device)
+    y_tensors = torch.tensor(y, dtype=torch.float32).view(-1, 1).to(device=device)
+    model = model.to(device=device)
 
-    for epoch in range(epochs):
-        model.train()
-        optimizer.zero_grad()
-        mean, variance = model(x_tensor)
+    batch_size = 32
+    dataset = torch.utils.data.TensorDataset(x_tensors, y_tensors)
+    loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True)
 
-        if fase_1:
-            loss = criterion(mean, y_tensor)
-        else:
-            loss = criterion_2(mean, y_tensor, variance)
+    model.train()
+    for epoch in tqdm(range(epochs), total=epochs):
+        results = []
+        for x_batch, y_batch in loader:
+            optimizer.zero_grad()
+            model_mean, model_variance = model(x_batch)
 
-        loss.backward()
-        optimizer.step()
-        print( f"Epoch {epoch}, Loss: {loss.item()}")
+            if fase_1:
+                loss = criterion(model_mean, y_batch)
+            else:
+                loss = criterion_2(model_mean, y_batch, model_variance)
+
+            results.append(loss.item())
+            loss.backward()
+            optimizer.step()
+        median_loss = statistics.median(results)
+        writer.add_scalar(f"Loss/train_{"MSE" if fase_1 else "GNLL Loss"}", median_loss, epoch)
+        print(f"Epoch {epoch}, Loss: {median_loss}")
 
     return model
+
 
 def Gaussian(x, mean, SD):
     dist = (1/(SD*np.sqrt(2*np.pi))) * np.exp(-0.5 * ((x - mean) / SD)**2)
@@ -76,23 +100,38 @@ def sample_distribution(x):
     dist =  x * np.sin(x) + Gaussian(x, Mean, SD_1) + Gaussian(x, Mean, SD_2) * x
     return dist
 
+
 def disentangle(mean, variance):
     epistemic = mean.var(dim=0)
     aleatoric = variance.mean(dim=0)
     total_uncertainty = epistemic + aleatoric
     return epistemic, aleatoric, total_uncertainty
 
+
+def enable_dropout(model):
+    for m in model.modules():
+        if isinstance(m, drop_connect):
+            m.inference = True
+
+
 def dropout_inference(model, x , T=50):
     model.eval()
-
-    def enable_dropout(model):
-        for m in model.modules():
-            if isinstance(m, drop_connect):
-                m.force_drop = True
-
     enable_dropout(model)
-    mean_predictions = torch.stack([model(x)[0] for _ in range(T)])
-    variance_predictions = torch.stack([model(x)[1] for _ in range(T)])
+    x = x.to(device=device)
+    
+    tot_mean_predictions = []
+    tot_variance_predictions = []
+    for x_i in range(x.shape[0]):
+        x_cur = x[x_i]
+        mean_predictions = []
+        variance_predictions = []
+        for _ in range(T):
+            mean_pred, var_pred = model(x_cur)
+            mean_predictions.append(mean_pred)
+            variance_predictions.append(var_pred)
+    
+    # TODO
+    # I don't know where to average the results, and how
     return disentangle(mean_predictions, variance_predictions)
 
 
@@ -100,17 +139,17 @@ def main():
     # Does the model learn the variance or the mean, cause otherwise we need to take the exp of the variance output?
     #  I assume the model is supposed to learn the distribution of the data?
 
+    # Experiment setup
     np.random.seed(42)
     torch.manual_seed(42)
-
     x = np.linspace(-10, 10, 1000)
     y = sample_distribution(x)
+    OOD = np.linspace(-20, 20, 1000)
 
+    # Create and train model
     model = regression_model()
     model = train_model(model, x, y, fase_1=True, epochs=100)
     model = train_model(model, x, y, fase_1=False, epochs=100)
-
-    OOD = np.linspace(-20, 20, 1000)
 
     epistemic, aleatoric, total_uncertainty = dropout_inference(model, torch.tensor(OOD, dtype=torch.float32).view(-1, 1))
 
